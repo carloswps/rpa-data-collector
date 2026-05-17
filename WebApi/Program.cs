750e9e13acf49872e7f49150e11c7698729b3a48
Carlos Wendell Pereira dos Santos <carloswendell@prodam.sp.gov.br>
2026-05-17 18:06:55 -0300

    chore: apply database migrations on application startup

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -71,6 +71,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
     db.Database.CanConnect();
 }
 
